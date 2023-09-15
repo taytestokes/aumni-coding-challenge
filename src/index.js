411bb2892dd092fcfeb938fcd6d2add869bec477
taytestokes <taytestokes9328@gmail.com>
2023-09-15 10:45:50 -0600

    Adds and configures tailwindcss for styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+// Tailwind Styles
+import "./styles/tailwind.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
