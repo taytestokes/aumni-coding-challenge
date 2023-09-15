d6391560e271ae74498a8acd597b221a2cc34d5c
taytestokes <taytestokes9328@gmail.com>
2023-09-15 12:39:02 -0600

    Initializes fund data for the authenticated user upon dashboard load

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { useAuth } from "../hooks/useAuth";
 
 import { DashboardLayout } from "../components/DashboardLayout";
 
+import funds from "../mocks/funds.json";
+
 const Dashboard = () => {
-  return <DashboardLayout>Dashboard Page!</DashboardLayout>;
+  const { user } = useAuth();
+
+  const [fundData, setFundData] = useState();
+
+  /**
+   * On the initial mount of the dashboard, we will fetch
+   * fund data for the user
+   */
+  useEffect(() => {
+    if (user) {
+      const fundData = funds.find((fund) => fund.userId === user.id);
+      setFundData(fundData);
+    }
+  }, []);
+
+  return (
+    <DashboardLayout>
+      {fundData?.companies.map((c) => (
+        <p key={c.id}>{c.name}</p>
+      ))}
+    </DashboardLayout>
+  );
 };
 
 export default Dashboard;
